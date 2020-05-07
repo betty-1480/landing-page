0fa6c1fc02b56cefa27b91936680c118dab2e0c3
betty <saviobetty@gmail.com>
2020-05-07 23:36:33 +1000

    Final Landing Page Project

--- js/app.js
+++ js/app.js
@@ -18,8 +18,8 @@
  * 
  * 
 */
-const navBar=document.getElementById('#navbar__list'); /* <ul id="navbar__list"> */
-const sectionCollection=document.getElementsByClassName('.landing__container');  /*<div class="landing__container">
+const navBar=document.getElementById('navbar__list'); /* <ul id="navbar__list"> */
+const sectionCollection=document.getElementsByClassName('landing__container');  /*<div class="landing__container">
                                                                                     <h2>Section 1</h2> */
 
 /**
@@ -31,10 +31,16 @@ function collectAllSectionNames(){
 /*     <ul id="navbar__list">
             <li class="navbar__menu"><a class ="menu__link" href="#section2">Landing Page</a></li>
         </ul> */    
-    for(const section in sectionCollection){
+    for(const section of sectionCollection){
+
+        const firstChild=section.firstElementChild;
+        const innerTxt=firstChild.innerText;
+
+        const anchorName="#"+section.parentElement.id;
+        
         const linkName=document.createElement('a'); /** <a> </a> */
-        linkName.innerText=section.firstElementChild.innerText;/** <a>Section 1 </a> */
-        linkName.setAttribute('href','#');/**  <a href="#">Section 1 </a> */
+        linkName.innerText=innerTxt/** <a>Section 1 </a> */
+        linkName.setAttribute('href',anchorName);/**  <a href="#">Section 1 </a> */
         linkName.setAttribute('class','menu__link');/**  <a class ="menu__link" href="#">Section 1 </a> */
         const liName=document.createElement('li');/** <li> </li> */
         liName.setAttribute('class','navbar__menu');/** <li class="navbar__menu"> </li> */
@@ -43,6 +49,16 @@ function collectAllSectionNames(){
     }
 }
 
+function respondToScrollToEvent(evt){
+   const clsList = document.getElementsByClassName('your-active-class');
+    if(clsList.length>0){
+        for(const cls of clsList){
+            cls.classList.toggle('your-active-class');
+        }   
+    }
+    const txt =  evt.target.innerText;
+    const ndName = document.querySelector(`[data-nav="${evt.target.innerText}"]`).classList.toggle('your-active-class');
+}
 
 /**
  * End Helper Functions
@@ -57,6 +73,7 @@ document.addEventListener('DOMContentLoaded', collectAllSectionNames);
 
 
 // Scroll to anchor ID using scrollTO event
+document.querySelector('#navbar__list').addEventListener("click",respondToScrollToEvent);
 
 
 /**
